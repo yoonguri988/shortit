5ca2926b52109ccccde5591847dff173347c3e2f
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-01-02 22:07:23 +0900

    3.리스폰스 다루기

--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -4,12 +4,32 @@ export default function handler(req, res) {
   // res.send(req.cookies);
   switch (req.method) {
     case "POST":
-      res.send("생성");
+      res.status(201).send({
+        title: "위키피디아 Next.js",
+        url: "https://en.wikipedia.org/wiki/Next.js",
+      });
       break;
     case "GET":
-      res.send("조회");
+      res.send([
+        {
+          id: "abc",
+          title: "위키피디아 Next.js",
+          url: "https://en.wikipedia.org/wiki/Next.js",
+        },
+        {
+          id: "def",
+          title: "코드잇 자유게시판",
+          url: "https://codeit.kr/community/general",
+        },
+        {
+          id: "ghi",
+          title: "코드잇 질문답변",
+          url: "https://www.codeit.kr/community/questions",
+        },
+      ]);
       break;
     default:
+      res.status(404).send();
       break;
   }
 }
