a0860c550150bfae04a79a436eb2a7b5f32d96cf
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-01-05 01:13:56 +0900

    Feat: 데이터 베이스 연동하기
    1. MongoAtlas 와 Mongoose 준비
    2. (Fix) short-links/index, short-links/[id]

--- README.md
+++ README.md
@@ -7,3 +7,9 @@
 - 리퀘스트 다루기
 - 리스폰스 다루기
 - (실습) API 라우트 만들기 - QRCode
+
+### 25.01.05 issue
+
+- MongoAtlas 와 Mongoose 준비
+- 데이터 베이스 연동하기
+- (Fix) short-links/index, short-links/[id]
--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -1,35 +1,15 @@
 export default function handler(req, res) {
-  // res.send(req.query);
-  // res.send(req.body);
-  // res.send(req.cookies);
-  switch (req.method) {
-    case "POST":
-      res.status(201).send({
-        title: "위키피디아 Next.js",
-        url: "https://en.wikipedia.org/wiki/Next.js",
-      });
+  switch(req.method) {
+    case 'PATCH':
+      res.send('ShortLink 수정');
       break;
-    case "GET":
-      res.send([
-        {
-          id: "abc",
-          title: "위키피디아 Next.js",
-          url: "https://en.wikipedia.org/wiki/Next.js",
-        },
-        {
-          id: "def",
-          title: "코드잇 자유게시판",
-          url: "https://codeit.kr/community/general",
-        },
-        {
-          id: "ghi",
-          title: "코드잇 질문답변",
-          url: "https://www.codeit.kr/community/questions",
-        },
-      ]);
+    
+    case 'GET':
+      res.send('ShortLink 조회');
       break;
+    
     default:
-      res.status(404).send();
+      res.send();
       break;
   }
 }
