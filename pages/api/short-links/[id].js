3f8f780427b4cb1e16694a1cf05831137a5b36a7
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-01-06 01:31:20 +0900

    Feat: 도큐먼트 수정, 삭제

--- pages/api/short-links/[id].js
+++ pages/api/short-links/[id].js
@@ -6,12 +6,20 @@ export default async function handler(req, res) {
   const { id } = req.query;
   switch (req.method) {
     case "PATCH":
-      res.send("ShortLink 수정");
+      const updatedShortLink = await ShortLink.findByIdAndUpdate(id, req.body, {
+        new: true,
+      });
+      res.send(updatedShortLink);
       break;
 
     case "GET":
       const shortLink = await ShortLink.findById(id);
       res.send(shortLink);
+      break;
+
+    case "DELETE":
+      await ShortLink.findByIdAndDelete(id);
+      res.status(204).send();
       break;
 
     default:
