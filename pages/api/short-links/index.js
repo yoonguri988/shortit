b252bc2929aa135c937d149907a195fcc506167d
yoonguri988 <56820897+yoonguri988@users.noreply.github.com>
2025-01-06 22:45:52 +0900

    Feat: 모델과 API 살펴보기
    1.주소를 짧은 주소로 변경하여 저장
    2.HASH key 값을 노출되지 않게 변경

--- pages/api/short-links/index.js
+++ pages/api/short-links/index.js
@@ -1,11 +1,18 @@
 import dbConnect from "@/db/dbConnect";
 import ShortLink from "@/db/models/ShortLink";
+import createShortURL from "@/lib/createShortURL";
 
 export default async function handler(req, res) {
   await dbConnect();
   switch (req.method) {
     case "POST":
-      const newShortLink = await ShortLink.create(req.body);
+      const { title, url } = req.body;
+      const shortUrl = createShortURL(url);
+      const newShortLink = await ShortLink.create({
+        title,
+        url,
+        shortUrl,
+      });
       res.status(201).send(newShortLink);
       break;
 
